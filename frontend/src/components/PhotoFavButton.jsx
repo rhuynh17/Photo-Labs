9c1078e0cde5a1b822b29204c780c0ee78e7e887
rhuynh17 <rhuynh1106@gmail.com>
2024-05-27 08:10:37 -0600

    fix: adjusted handleclick to accept previous statement

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -6,13 +6,13 @@ function PhotoFavButton() {
   const [isActive, setIsActive] = useState(false);
 
   const handleClick = () => {
-    setIsActive(!isActive);
+    setIsActive(prevIsActive => !prevIsActive);
   };
 
   return (
     <div className={`photo-list__fav-icon ${isActive ? 'active' : ''}`} onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        <FavIcon />
+        <FavIcon selected={isActive} />
       </div>
     </div>
   );
