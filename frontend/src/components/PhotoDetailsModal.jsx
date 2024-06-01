8114f674fed52f8ea6728c157d6b1f1073f2a028
rhuynh17 <rhuynh1106@gmail.com>
2024-05-31 21:50:49 -0600

    add: modal photo information passed

--- frontend/src/components/PhotoDetailsModal.jsx
+++ frontend/src/components/PhotoDetailsModal.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import '../styles/PhotoDetailsModal.scss';
 
-const PhotoDetailsModal = ({ isOpen, onClose }) => {
+const PhotoDetailsModal = ({ isOpen, onClose, photo }) => {
+  useEffect(() => {
+    if (photo) {
+      console.log(photo); 
+    }
+  }, [photo]);
+
   if (!isOpen) return null;
 
   return (
     <div className="photo-details-modal">
       <div className="photo-details-modal__content">
-        <button className="photo-details-modal__close-button" onClick={onClose}>×</button>
+      <button className="photo-details-modal__close-button" onClick={props.onClose}>
+        <img src={closeSymbol} alt="close symbol" />
+      </button>
       </div>
     </div>
   );
